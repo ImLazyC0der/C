a86390b484e9afca46bb0f576f60f49dbb7c9897
Anushree M <85032173+anushree1125@users.noreply.github.com>
2021-10-06 19:48:10 +0530

    Add files via upload

---
+++ simple programs/factorial-using_recursion.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int factorial(int n);
+
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    printf("Factorial of %d = %d", n, factorial(n));
+    return 0;
+}
+
+int factorial(int n) {
+    if (n>=1)
+        return n*factorial(n-1);
+    else
+        return 1;
+}
