d98d1cfeccf6cfab3619b37f97fa7575dc90f046
Anushree M <85032173+anushree1125@users.noreply.github.com>
2021-10-09 22:39:24 +0530

    Checking size of different pointer types

---
+++ simple programs/sizeOf_pointerVariables.c
@@ -0,0 +1,12 @@
+// C program to print size of different types of pointer variables.
+#include<stdio.h>
+
+int main(){
+  
+  printf("\nSize of char pointer: %d",sizeof(char*));
+  printf("\nSize of char pointer: %d",sizeof(int*));
+  printf("\nSize of char pointer: %d",sizeof(float*));
+  printf("\nSize of char pointer: %d",sizeof(long int*));
+  printf("\nSize of char pointer: %d\n",sizeof(double*));
+  return 0;
+}
