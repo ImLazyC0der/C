d13fbc500c562a555d8266e74379ceeea2d432f8
Rakshitha4 <92510957+Rakshitha4@users.noreply.github.com>
2021-10-18 19:59:04 +0530

    string.c

---
+++ C/string.c
@@ -0,0 +1,10 @@
+#include<stdio.h>  
+#include <string.h>    
+int main(){    
+  char ch[11]={'j', 'a', 'v', 'a', 't', 'p', 'o', 'i', 'n', 't', '\0'};    
+   char ch2[11]="javatpoint";    
+    
+   printf("Char Array Value is: %s\n", ch);    
+   printf("String Literal Value is: %s\n", ch2);    
+ return 0;    
+}    
